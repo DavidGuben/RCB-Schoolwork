74dcbe064b84324e20422e47267ce5bf4aa778ac
DavidGuben <davidjg6@gmail.com>
2016-09-07 21:10:49 -0400

    finishing react tests

---
+++ 18/webpack-react-starter/app/header.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Header = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>im an external header</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = Header;
